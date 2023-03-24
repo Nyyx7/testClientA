ddeecb55bd8ad1aba5b80eb8f716426b0c32290f
Nyyx7 <simo.med62@gmail.com>
2023-03-24 13:29:49 +0000

    Made various changes to improve performance

--- odoo_modules/my_module2/__manifest__.py
+++ odoo_modules/my_module2/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'My Module2',
     'version': '1.0',
-    'depends': ['base', 'mail'],
+    'depends': ['base', 'mail', 'crm'],
     'data': [
         'views/my_model_views.xml',
     ],
--- odoo_modules/my_module2/module2.py
+++ odoo_modules/my_module2/module2.py
@@ -1,6 +1,28 @@
-from odoo import fields, models
+from odoo import models, fields
 
-class CrmLead(models.Model):
+class Module2(models.Model):
     _inherit = 'crm.lead'
 
     date_fermeture = fields.Date(string='Date de fermeture')
+
+
+
+
+
+
+
+
+
+    """ @api.model
+    def create(self, vals):
+        lead = super(CrmLead, self).create(vals)
+        if lead.date_fermeture:
+            activity_type = self.env.ref('mail.mail_activity_data_todo')
+            activity = self.env['mail.activity'].create({
+                'activity_type_id': activity_type.id,
+                'summary': 'Reprendre contact',
+                'date_deadline': fields.Date.to_string(fields.Date.from_string(lead.date_fermeture) - relativedelta(months=1)),
+                'res_model_id': self.env['ir.model']._get('crm.lead').id,
+                'res_id': lead.id,
+            })
+        return lead """
